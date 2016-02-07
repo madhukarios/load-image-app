8ebb76104990563fecd7a1c61996dfc1a2e67250
Madhukar Bommala <madhukar.bomalla@gmail.com>
2016-02-07 13:10:56 -0700

    Initial Commit

---
+++ loadImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loadImage
+//
+//  Created by Madhukar Bommala on 2/7/16.
+//  Copyright © 2016 Madhukar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
