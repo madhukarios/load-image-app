c215641aa0db6f0bad78fcb52baff0b4a305fcd7
Madhukar Bommala <madhukar.bomalla@gmail.com>
2016-02-07 22:08:57 -0700

    Ver 1.0 Working code

--- loadImage/ViewController.swift
+++ loadImage/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+//import CoreLocation
 
 class ViewController: UIViewController {
+    
+//    var namloc : [(Int, CLLocationCoordinate2D)]
 
+    @IBOutlet weak var myName: UITextField!
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClick(sender: AnyObject) {
+        //switch myName.text(
+        
+        //case
+        //var name = myName.text?.lowercaseString
+        myImage.image=UIImage(named: (myName.text?.lowercaseString)!)
+        //myButton.titleLabel = "\(myName.text)"
+        //wait(10)
+        
+    }
 
+    @IBAction func myTextSelected(sender: AnyObject) {
+        myName.text=nil
+    }
 }
 
